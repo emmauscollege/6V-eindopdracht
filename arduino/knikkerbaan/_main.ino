753f51116d08c34664b627cd6d867fa50d0ff284
cam-emmauscollege <cam@emmauscollege.nl>
2021-10-25 09:02:09 +0000

    added Arduino sketch

---
+++ arduino/knikkerbaan/_main.ino
@@ -0,0 +1,79 @@
+#include <Arduino_JSON.h>
+
+KnikkerPoort poortBoven = KnikkerPoort (BOVEN_POORT_PIN, 0, 90);
+WiFiCommunicator wifi = WiFiCommunicator(WIFI_NETWERK, WIFI_WACHTWOORD, SERVER_DOMEIN);
+Teller tellerA = Teller(TELLER_A_PIN);
+
+int serverContactInterval = 15;                // 15 seconden
+unsigned long tijdVoorContactMetServer = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  wifi.begin();
+
+  wifi.stuurVerzoek("/api/set/nieuwerun", "");
+
+  poortBoven.open();
+}
+
+
+void loop() {
+  // laat de teller detecteren:
+  tellerA.update();
+
+  
+  // pauzeer de knikkerbaan als het tijd is voor contact met server
+  if (millis() > tijdVoorContactMetServer && poortBoven.getOpen()) {
+    poortBoven.sluit();
+  }
+
+  // knikkerbaan is leeggelopen, er zijn geen sensors dit iets moeten meten
+  // nu is het tijd om contact te leggen met de server:
+  if (millis() > tijdVoorContactMetServer + LEEGLOOP_TIJD) {
+    // contact met server houdt in:
+    //   * nieuw totaal aantal knikkers versturen
+    //   * instellingen ophalen
+
+    // maak de reeks variabelen voor achter de URL:
+    String data = "knikkers=";
+    data += 3; //tellerA.getTeller();
+
+    // als je meer waarden wilt toevoegen, ziet dat er zo uit:
+    //data += "&blabla";
+    //data += 5;
+
+    // stuur deze data naar het juiste adres
+    wifi.stuurVerzoek("/api/set/sensordata", data.c_str());
+
+    // vraag bij de server de nieuwe instellingen op:
+    String serverAntwoord = wifi.stuurVerzoek("/api/get/instellingen", "");
+    
+    // om een beeld te geven van het antwoord: print in seriële monitor:
+    Serial.println(serverAntwoord);
+
+    // om de instellingen gemakkelijk uit te kunnen lezen
+    JSONVar ontvangenInstellingen = JSON.parse(serverAntwoord);                      // deze regel hoef je niet te begrijpen
+
+    // check of de instellingen goed zijn binnengekomen
+    // indien ja: pas instellingen op Arduino aan
+    // indien nee: geef een foutmelding in de seriële monitor
+    if (JSON.typeof(ontvangenInstellingen) != "undefined") {                         // je hoeft deze voorwaarde niet te begrijpen
+      // stel de wachttijd (opnieuw) in:
+      serverContactInterval = ontvangenInstellingen["wachttijdPoort"];
+    }
+    else {
+      // evt. foutmelding:
+      Serial.println("FOUT: serverAntwoord kon niet worden verwerkt");
+    }
+
+    // servercommunicatie is afgerond
+    // bepaal nu op welke tijd de knikkerbaan
+    // opnieuw contact moet zoeken
+    unsigned long nu = millis();
+    tijdVoorContactMetServer = nu + (serverContactInterval * 1000);
+
+    // en zet nu het poortje weer open:
+    poortBoven.open();
+  }
+}
---
+++ arduino/knikkerbaan/knikkerbaan.ino
@@ -0,0 +1,14 @@
+
+
+//declareer hier je globale constanten:
+
+const int BOVEN_POORT_PIN = 7;
+const int BOVEN_POORT_WACHTTIJD = 500;
+const int LEEGLOOP_TIJD = 5000;
+
+char WIFI_NETWERK[] = "informaticaOnly";
+char WIFI_WACHTWOORD[] = "donaldduck";
+char SERVER_DOMEIN[] = "3000-amethyst-sailfish-xpjm3x26.ws-eu18.gitpod.io";
+//char SERVER_PAD[] = "/api/update";
+
+const int TELLER_A_PIN = 5;
